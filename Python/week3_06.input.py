53b60790740fbdc6bda54c81e06c32928fd04f37
202144046 <minjae54920@gmail.com>
2023-09-14 11:48:40 +0900

    2023-09-14 11:48

--- Python/week3_06.input.py
+++ Python/week3_06.input.py
@@ -1,9 +1,39 @@
 
 
+age= int(input("올해 나이 : "))
+print("내년 나이 : " + str(age + 1))
+
+age = int(input("올해 나이 : "))
+print("내년나이 :", age + 1)
+
+
+radius = input("반지름 : ")
+print("넓이 :", 3.14 * float(radius) * float(radius))
+print("--------------------------------------------------------------")
+
+radius = float(input("반지름 : "))
+print("넓이 :", 3.14 * radius * radius)
+print("--------------------------------------------------------------")
+
+
+a = int(1)
+a = int("1")
+a = int(1.1)
+# a = int("1.1")
+# a = int("안녕")
+
+a = float(1)
+a = float("1")
+a = float(1.1)
+a = float("1.1")
+
+
+
 print("---------------")
 age = input("나이 : ")
 # print(age + 1)
 # print(age + "1")
-print(int(age) + 1)
+age = int(age)
+print("내년 나이 :", age + 1)
 print("---------------")
 
