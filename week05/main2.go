a2ae3ad80989e1cf5c8e2d3b17fa2984143874cf
202144046 <minjae54920@gmail.com>
2023-09-25 17:18:34 +0900

    week05 v0.6

--- week05/main2.go
+++ week05/main2.go
@@ -3,14 +3,17 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
 	fmt.Print("Input score : ")
 	reader := bufio.NewReader(os.Stdin)
-	// inputScore := reader.ReadString('\n') // 1 variable but reader.ReadString returns 2 values
-	// inputScore, err := reader.ReadString('\n') // err declared and not used
-	inputScore, _ := reader.ReadString('\n')
+	// inputScore := reader.ReadString('\n') 		// 1 variable but reader.ReadString returns 2 values
+	// inputScore, err := reader.ReadString('\n') 	// err declared and not used
+	// inputScore, _ := reader.ReadString('\n') 	// option 1
+	inputScore, err := reader.ReadString('\n') // option 2
+	log.Fatal(err)                             // 프로그램 종료 (에러 감지 후 리포팅 하기 위해 사용)
 	fmt.Println(inputScore)
 }
