cf5d8060a0af1928ddbcc411819bd8ce3bc47ec7
202144046 <minjae54920@gmail.com>
2023-10-05 12:27:27 +0900

    2023-10-05 12:27

---
+++ Python/week6_04.etc_1.py
@@ -0,0 +1,12 @@
+# p. 251
+
+scores = [100, 45, 80, 0, 23]
+
+print(min(scores))
+print(max(scores))
+print(sum(scores))
+print(sum(scores)/len(scores)) # 정수형 나누기
+
+# 사용자가 입력한 점수를 가지고
+# 평균을 구하는 코드를 작성하세요.
+
