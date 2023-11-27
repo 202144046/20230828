7d94e38b619632bd1083ec320e609d8d198cf8ae
202144046 <minjae54920@gmail.com>
2023-11-27 17:08:16 +0900

    2023-11-27 17:08

--- Go/week14/main.go
+++ Go/week14/main.go
@@ -4,32 +4,73 @@ import (
 	"fmt"
 )
 
+// func main() {
+// 	// // var games map[int]string
+// 	// // games = make(map[int]string)
+// 	// games := make(map[int]string(
+// 	// 	456: "성기훈",
+// 	// 	218: "박해수",
+// 	// 	067: "강새벽",
+// 	// 	001: "오일남",
+// 	// 	199: "알리"))
+
+// 	// // append
+// 	// // games[456] = "성기훈"
+// 	// // games[218] = "박해수"
+// 	// // games[067] = "강새벽"
+// 	// // games[001] = "오일남"
+// 	// // games[199] = "알리"
+// 	// // games[101] = "아이오아이"
+
+// 	// // fmt.Println((games[067]))
+// 	// for _, v := range games {
+// 	// 	fmt.Println(v)
+// 	// }
+
+// 	// // update
+// 	// games[101] = "장덕수"
+// 	// for k, v := range games {
+// 	// 	fmt.Println(k, v)
+// 	// }
+
+// 	// // delete
+// 	// delete(games, 199)
+// 	// for k, v := range games {
+// 	// 	fmt.Println(k, v)
+// 	// }
+
+// }
+
+
+
+// func main() {
+// 	// balls := make(map[string]int)
+// 	// // var balls map[string]int
+// 	// fmt.Printf("$#v\n", balls)
+// 	// balls["성기훈"] = 20
+// 	// fmt.Println(balls["성기훈"])
+// 	fmt.Println(balls["오일남"])
+// }
+
+
+
+func status(name string) {
+	balls := map[string]int("성기훈" : 20, "오일남" : 0)
+	// ball := balls[name]
+	ball, exists := balls[name]
+	if !exists{
+		fmt.Println(name, "님은 게임에 참여하실 수 없습니다.")
+	
+		}else if ball < 1 {
+			fmt.Println(name, "남은", balls[name], "개로 탈락")
+		
+	 		}else{
+				fmt.Println(name, "남은 게임에서 승리하셨습니다.")
+	
+ } } 
+
 func main() {
-	// var games map[int]string
-	// games = make(map[int]string)
-	games := make(map[int]string)
-	// append
-	games[456] = "성기훈"
-	games[218] = "박해수"
-	games[067] = "강새벽"
-	games[001] = "오일남"
-	games[199] = "알리"
-	games[101] = "아이오아이"
-
-	// fmt.Println((games[067]))
-	for _, v := range games {
-		fmt.Println(v)
-	}
-
-	// update
-	games[101] = "장덕수"
-	for k, v := range games {
-		fmt.Println(k, v)
-	}
-
-	// delete
-	delete(games, 199)
-	for k, v := range games {
-		fmt.Println(k, v)
-	}
+	status("오일남")
+	status("강철")
+	status("성기훈")
 }
